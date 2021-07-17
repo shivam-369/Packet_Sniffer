9774a7c96d8a231fae1ae7e8472a5de28d1a9b3b
Shivam Dubey <dubeyshivam1101@gmail.com>
2021-07-17 11:24:29 +0530

    Added a tab_count as a parameter in evry function for proper formatting in different situations. Checksum is printed in Hexadecimal form.

--- UDP.py
+++ UDP.py
@@ -1,15 +1,23 @@
 import struct
+from formatting import *
 
-def UDP_packet(data, total_length):
+def UDP_packet(data, total_length, tab_count):
 	source_port, destination_port = struct.unpack("! H H", data[:4])
-	UDP_Datagram_length, checksum = struct.unpack("! H h", data[4:8])
+	UDP_Datagram_length, checksum = struct.unpack("! H H", data[4:8])
 	
-	print("\n\t\tUDP Datagram:")
-	print(f"\t\t\tSource port: {source_port}")
-	print(f"\t\t\tDestination port: {destination_port}")
-	print(f"\t\t\tUDP Datagram length: {UDP_Datagram_length}")
-	print(f"\t\t\tChecksum: {checksum}")
+	print("\n")
+	print_tabs(tab_count)
+	print("UDP Datagram:")
+	print_tabs(tab_count + 1)
+	print(f"Source port: {source_port}")
+	print_tabs(tab_count + 1)
+	print(f"Destination port: {destination_port}")
+	print_tabs(tab_count + 1)
+	print(f"UDP Datagram length: {UDP_Datagram_length}")
+	print_tabs(tab_count + 1)
+	print(f"Checksum: {hex(checksum)}({checksum})")
 	
 	return data[8:]
+	
 	
 	
