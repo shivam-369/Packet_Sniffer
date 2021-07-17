36f62129b433cb6d958bf3cf365df749637791ae
Shivam Dubey <dubeyshivam1101@gmail.com>
2021-07-17 11:33:46 +0530

    Added tab_count for formatting.

--- ethernet_frame.py
+++ ethernet_frame.py
@@ -1,20 +1,23 @@
 import struct
-
+from formatting import *
 
 def ethernet_address_format(addr):
 	proper_addr = map('{:02x}'.format, addr)
 	proper_mac_addr = ':'.join(proper_addr)
 	return proper_mac_addr
 
-def ethernet_frame(data):
+def ethernet_frame(data, tab_count):
 	destination, source = struct.unpack("! 6s 6s", data[0: 12])
 	protocol = struct.unpack("! h", data[12: 14])	
 	eth_proto = hex(protocol[0])
 	eth_destination = ethernet_address_format(destination)
 	eth_source = ethernet_address_format(source)
 	print("\nEthernet Frame:")
-	print(f"\tDestination: {eth_destination}")
-	print(f"\tSource: {eth_source}")
-	print(f"\tProtocol: {eth_proto}")
+	print_tabs(tab_count + 1)
+	print(f"Destination: {eth_destination}")
+	print_tabs(tab_count + 1)
+	print(f"Source: {eth_source}")
+	print_tabs(tab_count + 1)
+	print(f"Protocol: {eth_proto}")
 	return eth_proto, data[14: ]
 	
--- ipv4.py
+++ ipv4.py
@@ -1,19 +1,26 @@
 import struct
-
+from formatting import *
 
 #Prints values of IP flags
-def IP_flags(flags):
+def IP_flags(flags, tab_count):
 	x_bit = bool(flags >> 2)
 	Dont_fragment_bit = bool(flags & (1 << 1))	
 	More_Fragments_bit = bool(flags & 1)
 	
-	print("\t\tIP Flags:")
-	print(f"\t\t\tReserved Bit: {x_bit}")
-	print(f"\t\t\tDon't Fragment Bit: {Dont_fragment_bit}")
-	print(f"\t\t\tMore Fragments Bit: {More_Fragments_bit}")	
+	print_tabs(tab_count)
+	print("IP Flags:")
+	print_tabs(tab_count + 1)
+	print(f"Reserved Bit: {x_bit}")
+	print_tabs(tab_count + 1)
+	print(f"Don't Fragment Bit: {Dont_fragment_bit}")
+	print_tabs(tab_count + 1)
+	print(f"More Fragments Bit: {More_Fragments_bit}")	
+
+
+
 
 #Determines the type of service from IP header
-def Type_Of_Service(Type_of_service):
+def Type_Of_Service(Type_of_service, tab_count):
 	precedence_list = {0: "routine", 1 : "Priority" , 2 : "Immediate", 3 : "Flash", 4 : "Flash Override", 5 : "CRITIC/ECP", 6 : "Internetwork Control", 7 : "Network Control"}
 	Delay_list = {0 : "Normal Delay", 1 : "Low Delay"}
 	Throughput_list = {0: "Normal Throughput", 1: "High Throughput"}
@@ -25,15 +32,21 @@ def Type_Of_Service(Type_of_service):
 	Reliability_bit = (Type_of_service & (1 << 2))
 	Reserved_bits = (Type_of_service & 3)
 	
-	print("\t\tService bits:")
-	print(f"\t\t\tPrecedence Bits: {precedence_bit}({precedence_list[precedence_bit]})")
-	print(f"\t\t\tDelay Bit: {Delay_bit}({Delay_list[Delay_bit]})")
-	print(f"\t\t\tThroughput Bit: {Throughput_bit}({Throughput_list[Throughput_bit]})")
-	print(f"\t\t\tRelaibility Bit: {Reliability_bit}({Reliability_list[Reliability_bit]})")
+	print_tabs(tab_count)
+	print("Service bits:")
+	print_tabs(tab_count + 1)
+	print(f"Precedence Bits: {precedence_bit}({precedence_list[precedence_bit]})")
+	print_tabs(tab_count + 1)
+	print(f"Delay Bit: {Delay_bit}({Delay_list[Delay_bit]})")
+	print_tabs(tab_count + 1)
+	print(f"Throughput Bit: {Throughput_bit}({Throughput_list[Throughput_bit]})")
+	print_tabs(tab_count + 1)
+	print(f"Relaibility Bit: {Reliability_bit}({Reliability_list[Reliability_bit]})")
 	
+
 	
 #prints Differentiated Service field 
-def Diff_service_fields(service):
+def Diff_service_fields(service, tab_count):
 	
 	''' DSCP - Differentiated service Codepoint
 	    ECN  - Explicit Congestion Notification
@@ -41,49 +54,67 @@ def Diff_service_fields(service):
 	DSCP = (service >> 2)
 	ECN = (service & 3)
 	
-	print(f"\t\tDifferentiated Field: {service}")
-	print(f"\t\t\tDifferentiated Service Codepoint: {DSCP}({format(DSCP, '#06')})")
-	print(f"\t\t\tExplicit Congestion Notification: {ECN}({format(ECN, '#02')})")
+	print_tabs(tab_count)
+	print(f"Differentiated Field: {service}")
+	print_tabs(tab_count + 1)
+	print(f"Differentiated Service Codepoint: {DSCP}({format(DSCP, '#06')})")
+	print_tabs(tab_count + 1)
+	print(f"Explicit Congestion Notification: {ECN}({format(ECN, '#02')})")
 	
-
 
 # Returns Properly formatted IP address	
 def IPv4_address_format(addr):
 	return '.'.join(map(str, addr))
 	
 	
-#Unpacks IP packet	
-def IPv4_packet(data):
+	
+#Unpacks IP packet. Same as above function but it returns data and other variables instead of header_length	
+def IPv4_packet(data, tab_count):
+	
 	version_and_Hlength, Type_of_service, total_length = struct.unpack("! B B h", data[:4])
 	version = (version_and_Hlength >> 4)
 	header_length = (version_and_Hlength & 15) * 4
-	identification, flags_and_offset = struct.unpack("! h h", data[4: 8])
+	
+	identification, flags_and_offset = struct.unpack("! H H", data[4: 8])
 	flags = (flags_and_offset >> 13)
 	fragment_offset = ((flags_and_offset) & (2 ** 13))
-	TTL, IP_proto, header_checksum = struct.unpack("! B B h", data[8: 12])
+	
+	TTL, IP_proto, header_checksum = struct.unpack("! B B H", data[8: 12])
+	
 	source, destination =struct.unpack("! 12x 4s 4s", data[: 20])
 	ipv4_source = IPv4_address_format(source)
 	ipv4_destination = IPv4_address_format(destination)
 	
-	print("\n\tIPv4 packet:")
-	print(f"\t\tversion: {version}")
-	print(f"\t\tHeader length: {header_length}")
+	print("\n")
+	print_tabs(tab_count)
+	print("IPv4 packet:")
+	print_tabs(tab_count + 1)
+	print(f"version: {version}")
+	print_tabs(tab_count + 1)
+	print(f"Header length: {header_length}")
 	
 	#print(f"\t\tDifferentiated Service Field: {Type_of_service}")
-	Diff_service_fields(Type_of_service)
+	Diff_service_fields(Type_of_service, tab_count + 1)
 	
 	#Type_Of_Service(Type_of_service)
-	
-	print(f"\t\tTotal Length: {total_length}")
-	print(f"\t\tidentification: {identification}")
+	print_tabs(tab_count + 1)
+	print(f"Total Length: {total_length}")
+	print_tabs(tab_count + 1)
+	print(f"identification: {identification}")
 	#print(f"\t\tflags: {flags}")
-	IP_flags(flags)
-	print(f"\t\tfragment offset: {fragment_offset}")
-	print(f"\t\tTime to Leave: {TTL}")
-	print(f"\t\tprotocol: {IP_proto}")
-	print(f"\t\theader checksum: {header_checksum}")	
-	print(f"\t\tSource: {ipv4_source}")
-	print(f"\t\tDestination: {ipv4_destination}")
+	IP_flags(flags, tab_count + 1)
+	print_tabs(tab_count + 1)
+	print(f"fragment offset: {fragment_offset}")
+	print_tabs(tab_count + 1)
+	print(f"Time to Leave: {TTL}")
+	print_tabs(tab_count + 1)
+	print(f"protocol: {IP_proto}")
+	print_tabs(tab_count + 1)
+	print(f"header checksum: {hex(header_checksum)}({header_checksum})")	
+	print_tabs(tab_count + 1)
+	print(f"Source: {ipv4_source}")
+	print_tabs(tab_count + 1)
+	print(f"Destination: {ipv4_destination}")
 	
 	return IP_proto, total_length - header_length, data[header_length:]	 
 
